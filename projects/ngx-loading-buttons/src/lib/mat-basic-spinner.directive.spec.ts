5d73815f2552b9d3772f172ff38895634b1d69c1
Daniel Kreider <rxgmt61of@mozmail.com>
2022-04-11 08:58:52 -0300

    Create some basic tests for the mat spinner

--- projects/ngx-loading-buttons/src/lib/mat-basic-spinner.directive.spec.ts
+++ projects/ngx-loading-buttons/src/lib/mat-basic-spinner.directive.spec.ts
@@ -1,8 +1,40 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from 'projects/ngx-loading-buttons-playground/src/app/app.component';
 import { MatBasicSpinnerDirective } from './mat-basic-spinner.directive';
 
-describe('SpinnerDirective', () => {
+describe('MatBasicSpinnerDirective', () => {
+
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [AppComponent, MatBasicSpinnerDirective]
+    })
+    .createComponent(AppComponent);
+
+    fixture.detectChanges();
+  })
+
   it('should create an instance', () => {
     const directive = new MatBasicSpinnerDirective();
     expect(directive).toBeTruthy();
   });
+
+  it('should show spinner and text when loading is true', () => {
+    fixture.componentInstance.loading = true;
+    fixture.detectChanges();
+
+    var element: HTMLElement = fixture.nativeElement.querySelector("#spinnerWithText");
+    expect(element.className).toEqual("mat-spinner");
+  });
+
+  it('should show spinner and hide text when loading is true', () => {
+    fixture.componentInstance.loading = true;
+    fixture.detectChanges();
+
+    var element: HTMLElement = fixture.nativeElement.querySelector("#spinnerWithoutText");
+    expect(element.className).toContain("mat-spinner");
+    expect(element.className).toContain("hide-btn-text");
+  });
+
 });
