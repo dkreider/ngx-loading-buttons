a0c2257fb217aada6b790a8ecf38ca675e9573ab
Daniel Kreider <rxgmt61of@mozmail.com>
2022-04-11 20:47:04 -0300

    Only add CSS style if it is missing

--- projects/ngx-loading-buttons/schematics/ng-add/setup.ts
+++ projects/ngx-loading-buttons/schematics/ng-add/setup.ts
@@ -3,6 +3,8 @@ import { getProjectFromWorkspace } from '@angular/cdk/schematics';
 import { getProjectTargetOptions } from '@angular/cdk/schematics';
 import { updateWorkspace} from '@schematics/angular/utility/workspace';
 
+const themePath = `./node_modules/ngx-loading-buttons/src/styles.css`;
+
 export function cssAdd (options: any): Rule {
   return async () => {
     return chain([
@@ -12,7 +14,6 @@ export function cssAdd (options: any): Rule {
 }
 
 function insertCSSDependency(project: string): Rule {
-    const themePath = `./node_modules/ngx-loading-buttons/src/styles.css`;
     return chain([
       addThemeStyleToTarget(project, 'build', themePath),
       addThemeStyleToTarget(project, 'test', themePath),    
@@ -21,9 +22,17 @@ function insertCSSDependency(project: string): Rule {
   
 function addThemeStyleToTarget(projectName: string, targetName: 'test' | 'build', assetPath: string): Rule {
     return updateWorkspace(workspace => {
-      const project = getProjectFromWorkspace(workspace, projectName);  
-      const targetOptions = getProjectTargetOptions(project, targetName);
-      const styles = targetOptions['styles'] as (string | {input: string})[];
-      styles.unshift(assetPath);
+        const project = getProjectFromWorkspace(workspace, projectName);  
+        const targetOptions = getProjectTargetOptions(project, targetName);
+        const styles = targetOptions['styles'] as (string | {input: string})[];
+
+        const existingStyles = styles.map(s => (typeof s === 'string' ? s : s.input));
+
+        for (let [, stylePath] of existingStyles.entries()) {
+            if (stylePath === assetPath)
+                return;
+        }
+
+        styles.unshift(assetPath);
     });
 }
